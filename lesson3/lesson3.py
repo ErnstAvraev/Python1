b87361ad6ac0076373cc5b27d80b3712b35df608
Ernst Avraev <avraev.ernst@yandex.ru>
2023-03-02 19:08:56 +0600

    commented some code

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -51,12 +51,12 @@
 # #                           Cортировка слиянием
 
 # def merge_sort(nums):
-#     if len(nums) > 1:
-#         mid = len(nums)//2
-#         left = nums[:mid]
-#         right = nums[mid:]
-#         merge_sort(left)
-#         merge_sort(right)
+    # if len(nums) > 1:
+    #     mid = len(nums)//2
+    #     left = nums[:mid]
+    #     right = nums[mid:]
+    #     merge_sort(left)
+    #     merge_sort(right)
 #         i = j = k = 0
 #         while i < len(left) and j < len(right):
 #             if left[i] < right[j]:
