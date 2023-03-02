a18ff1cc45a8aa098a01d32aff641420690b4c27
Ernst Avraev <avraev.ernst@yandex.ru>
2023-03-02 18:58:03 +0600

    HW is finished

--- seminar7/seminar7HW.py
+++ seminar7/seminar7HW.py
@@ -47,18 +47,67 @@
 # print_operation_table(operation,num_rows,num_columns)
 
 from random import *
-films = []
-films.append("Матрица")
-films.append("Солярис")
-films.append("Властелин колец")
-films.append("Техасская резня бензопилой")
-films.append("Санта Барбара")
-while True:
-    command = input("Введите команду: ")
-    if command == "/start":
-        print("Бот фильмотека начал свою работу")
-    elif command == "/stop":
-        print("Бот остановил свою работу. Заходите еще, будем рады!)")
-        break
-    else:
-        print("Неопознаная команда. Просьба изучить мануал через /help")
+import json
+
+# films = []
+# names = []
+
+# def save():
+#     with open("list_of_names.json", "w", encoding="utf-8") as n:
+#         n.write(json.dumps(names, ensure_ascii=False))
+
+
+
+# with open("list_of_names.json", "r", encoding="utf-8") as n:
+#     names = json.load(n)
+#     print("Бот начал работу. Поприветствуйте бота)")
+# while True:
+#     text = input(": ").lower()
+#     if text == "привет" or text == "здравствуйте" or text == "здравствуй":
+#         print("Здравствуйте! Меня ховут Чатик) Как вас зовут?")
+#         name = input(': ')
+#         if name in names:
+#             print("Мы уже знакомы)")
+#         else:
+#             print(f'Приятно познакомиться {name}!')
+#             names.append(name)
+#             save()
+#     elif text == "как дела?":
+#         print("Как всегда отлично! Работаю без сучка, без задоринки)")
+#         print("Как ваши дела?")
+#         hru = input(": ").lower()
+#         if hru == "отлично" or hru == "хорошо" or hru == "шикарно":
+#             print("Я очень рад за вас!")
+#             print("Раскажите как прошел ваш день?")
+#             how_day = str(input(": ").lower())
+#             if len(how_day) > 20:
+#                 print("Какой насыщенный день!)")
+#             else:
+#                 print("Рад за вас)")
+#         elif hru == "плохо" or hru == "ужасно" or hru == "не очень":
+#             print("Мне очень жаль(\n"
+#                 "Чем я могу вам помочь?\n"
+#                 "Могу ли я предложить вам фильм, чтобы вас развеселить?")
+#             question1 = input(": ").lower()
+#             if question1 == "да" or "пожалуй":
+#                 with open("films.json", "r", encoding="utf-8") as fh:
+#                     films = json.load(fh)
+#                     print("Слепой жребий показал вам фильм - " + choice(films))
+#     elif text == "посоветуй фильм":
+#         with open("films.json", "r", encoding="utf-8") as fh:
+#                     films = json.load(fh)
+#                     print("Слепой жребий показал вам фильм - " + choice(films))
+#     elif text == "спасибо":
+#         print("Всегда рад помочь!)")
+#     elif text =="/help":
+#         print("привет - запускает бот\n"
+#               "пока - останавливает бот\n"
+#               "как дела? - поинтересуется вашими делами)\n"
+#               "посоветуй фильм - порекоментует фильм\n"
+#               "спасибо - благодарность)")
+#     elif text == "пока":
+#         print("Пока, до скорой встречи)")
+#         save()
+#         break
+#     else:
+#         print("Неопознаная команда. Просьба изучить мануал через /help")
